5889191da0a4eef3af249c19012e15ab62d779d1
davidguy3237 <davidtran3237@gmail.com>
2023-04-10 11:18:09 -0700

    Complete React Components Exercise

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// TODO
+const {useState} = React;
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const style = {
+    fontWeight: isHovering ? 'bold' : 'normal'
+  };
+
+  return (
+    <li
+    style={style}
+    onMouseEnter={() => setIsHovering(!isHovering)}
+    onMouseLeave={() => setIsHovering(!isHovering)}
+    >
+      {props.item}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList items={['stuff', 'more stuff', 'even more stuff']}/>, document.getElementById('app'));
